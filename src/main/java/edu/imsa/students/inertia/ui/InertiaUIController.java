3e1f194dec184c9035f3d100494227272aa0d17b
J0Nreynolds <j14reynolds@gmail.com>
2014-09-18 22:28:21 -0500

    With this commit comes a stable setup for the UI. The InertiaApplication starts the application and sets the layout specified in the InerialUI.fxml file. The InertiaUIController allows for methods to be made for UI component actions specified in the InertialUI.fxml file.

---
+++ src/main/java/edu/imsa/students/inertia/ui/InertiaUIController.java
@@ -0,0 +1,22 @@
+package edu.imsa.students.inertia.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class InertiaUIController {
+
+	@FXML
+	Button play;
+	
+	
+	private void playSimulation(){
+		
+	}
+	
+	private void pauseSimulation(){
+	}
+	
+	
+	
+}
