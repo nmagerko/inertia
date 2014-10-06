6747b184890374f621fd01b89455e6829d546ffc
J0Nreynolds <j14reynolds@gmail.com>
2014-10-05 21:50:50 -0500

    Added empty methods for each UI component action.

--- src/main/java/edu/imsa/students/inertia/ui/InertiaUIController.java
+++ src/main/java/edu/imsa/students/inertia/ui/InertiaUIController.java
@@ -1,22 +1,93 @@
 package edu.imsa.students.inertia.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class InertiaUIController {
 
+	boolean paused = true;
 	@FXML
-	Button play;
-	
-	
-	private void playSimulation(){
-		
+	ToggleButton play;
+	@FXML
+	ImageView image;
+	@FXML
+	Canvas canvas;
+	@FXML
+	Slider timeSlider;
+	@FXML
+	MenuItem copyMenuItem;
+	@FXML
+	MenuItem deleteMenuItem;
+	@FXML
+	MenuItem closeMenuItem;
+
+	@FXML
+	private void playPauseSimulation() {
+		System.out.println("The play/pause button works.");
+		// if (paused) {
+		// image.setImage(new Image("src/main/resources/ui/img/pause.png"));
+		// paused = false;
+		// } else {
+		// image.setImage(new Image("src/main/resources/ui/img/pause.png"));
+		// paused = true;
+		// }
 	}
-	
-	private void pauseSimulation(){
+
+	@FXML
+	private void handleNextButton() {
+		System.out.println("This works");
 	}
-	
-	
-	
+
+	@FXML
+	private void handlePreviousButton() {
+
+		System.out.println("This works");
+	}
+
+	@FXML
+	private void copy() {
+
+		System.out.println("This works");
+	}
+
+	@FXML
+	private void delete() {
+		System.out.println("This works");
+	}
+
+	@FXML
+	private void closeProgram() {
+
+		System.out.println("This works");
+	}
+
+	@FXML
+	private void changeTime() {
+
+		System.out.println("This works");
+	}
+
+	@FXML
+	private void checkObjectsLocations() {
+
+		System.out.println("This works");
+	}
+
+	@FXML
+	private void checkObjectsDrag() {
+
+		System.out.println("This works");
+	}
+
+	@FXML
+	private void releaseObject() {
+		System.out.println("This works");
+	}
+
 }
